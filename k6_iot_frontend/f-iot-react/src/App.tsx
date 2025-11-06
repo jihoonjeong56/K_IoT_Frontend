ddb3cb4792b8cb9719db5c521b1d809e58ef127f
jihoonjeong56 <jeong283056@gmail.com>
2025-11-06 11:01:22 +0900

    add practice Products dashboard

--- k6_iot_frontend/f-iot-react/src/App.tsx
+++ k6_iot_frontend/f-iot-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Basic from "@/pages/a_basic";
 import RoutePages from "@/pages/b_Route";
@@ -7,10 +7,13 @@ import Navibar from "./components/NaviBar";
 import PostList from "./_practices/a_basic/PostList";
 import PostDetail from "./components/PostDetail";
 import SearchApp from "./_practices/c_hooks/SearchApp";
+import Z_Products from "./pages/b_Route/Z_Products";
+import Z_ProductDetail from "./pages/b_Route/Z_ProductDetail";
+import Z_ProductInfo from "./pages/b_Route/Z_ProductInfo";
+import Z_ProductReviews from "./pages/b_Route/Z_ProductReviews";
+import Z_DashBoard from "./pages/b_Route/Z_DashBoard";
 
 // 파일명 없으면 무조건! 해당 파일의 index 라는 이름의 파일을 가져옴
-
-
 
 function App() {
   return (
@@ -33,6 +36,16 @@ function App() {
         <Route path="/practice/post" element={<PostList />} />
         <Route path="/practice/post/:id" element={<PostDetail />} />
         <Route path="/practice/search" element={<SearchApp />} />
+
+        {/* pages/b_route - z_실습코드 */}
+        <Route path="/" element={<Navigate to="/products" />} />
+        <Route path="/products" element={<Z_Products />} />
+        <Route path="/products/:id" element={<Z_ProductDetail />}>
+          {/* 중첩 라우팅: 상대경로 */}
+          <Route path="info" element={<Z_ProductInfo />} />
+          <Route path="reviews" element={<Z_ProductReviews />} />
+        </Route>
+        <Route path="/dashboard" element={<Z_DashBoard />} />
       </Routes>
     </>
   );
---
+++ k6_iot_frontend/f-iot-react/src/pages/b_Route/Z_ProductInfo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Z_ProductInfo() {
+  return (
+    <div>
+      <h4>제품 상세 정보</h4>
+      <ul>
+        <li>브랜드 : A브랜드</li>
+        <li>가격: 10000원</li>
+        <li>제고: 10개</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Z_ProductInfo;
